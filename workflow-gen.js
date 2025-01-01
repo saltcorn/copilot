3cdc59a39bcc1723b4d85f0119e3ffca952fa383
Tom Nielsen <tanielsen@gmail.com>
2025-01-01 12:44:12 +0000

    return a dummy workflow

--- workflow-gen.js
+++ workflow-gen.js
@@ -2,10 +2,23 @@ const { getState } = require("@saltcorn/data/db/state");
 
 module.exports = {
   run: async (description, trigger_id) => {
-    const gen = getState().functions.llm_generate.run(description, {
+    /*const gen = getState().functions.llm_generate.run(description, {
       systemPrompt: `You are a helpful code assistant.`,
-    });
-    return [];
+    });*/
+    const rnd = Math.round(100 * Math.random());
+    return [
+      {
+        name: "step1",
+        trigger_id,
+        next_step: "",
+        only_if: "",
+        action_name: "SetContext",
+        initial_step: true,
+        configuration: {
+          ctx_values: `{x: ${rnd}}`,
+        },
+      },
+    ];
   },
   isAsync: true,
   description: "Generate a workflow",
