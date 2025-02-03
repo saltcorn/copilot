0e0d4399a77d6b5cfcaa4f5709af17238a1a3b77
Tom Nielsen <tanielsen@gmail.com>
2025-02-03 11:46:43 +0000

    right border on sessions

--- chat-copilot.js
+++ chat-copilot.js
@@ -140,7 +140,7 @@ const run = async (table_id, viewname, cfg, state, { res, req }) => {
   );
   return {
     widths: [3, 9],
-    gx: 4,
+    gx: 3,
     besides: [
       {
         type: "container",
@@ -201,7 +201,7 @@ const run = async (table_id, viewname, cfg, state, { res, req }) => {
               div.interaction-segment {padding-top: 5px;padding-bottom: 5px;}
               div.interaction-segment p {margin-bottom: 0px;}
               div.interaction-segment div.card {margin-top: 0.5rem;}
-            div.prevcopilotrun {border-top: 1px solid #e7e7e7;padding-top:3px; padding-bottom:3px}
+            div.prevcopilotrun {border-top: 1px solid #e7e7e7;border-right: 1px solid #e7e7e7;padding-top:3px; padding-bottom:3px; padding-right: 1rem;}
             div.prevcopilotrun:hover {cursor: pointer}
             .copilot-entry .submit-button:hover { cursor: pointer}
 
