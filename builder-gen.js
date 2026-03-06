8a7cce36cca6bc8a6084f7c400fa60e98d7a474e
Tom Nielsen <tanielsen@gmail.com>
2026-03-06 16:09:17 +0000

    copilot_generate_layout (html only)

--- builder-gen.js
+++ builder-gen.js
@@ -6,15 +6,26 @@ const { getActionConfigFields } = require("@saltcorn/data/plugin-helper");
 
 module.exports = {
   run: async (prompt, mode, table) => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 5000);
-    });
+    const str = await getState().functions.llm_generate.run(
+      `Generate an HTML snippet according to the requirement below. Your snippet will be 
+placed inside a page which has loaded the Bootstrap 5 CSS framework, so you can use any 
+Bootstrap 5 classes.
+
+If you need to run javascript in script tag that depends on external reosurces, you wrap this
+in a DOMContentLoaded event handler as external javascript resources may be loaded after your HTML snippet is included.
+
+Include only the HTML snippet with no explanation before or after the code snippet.
+      
+Generate the HTML5 snippet for this request: ${prompt}
+`,
+    );
+    const strHtml = str.includes("```html")
+      ? str.split("```html")[1].split("```")[0]
+      : str;
     return {
       type: "blank",
       isHTML: true,
-      contents: `<h4>Placeholder copilot response</h4>
-      <blockquote class="blockquote"><p>${prompt}</p></blockquote>
-      <pre>mode=${mode} table=${table}</pre>`,
+      contents: strHtml,
       text_strings: [],
     };
   },
