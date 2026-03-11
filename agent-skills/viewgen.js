94afbae8876c67d5161abd981a29aba22a2cf141
Tom Nielsen <tanielsen@gmail.com>
2026-03-11 21:38:54 +0000

    basoc view gen

--- agent-skills/viewgen.js
+++ agent-skills/viewgen.js
@@ -1,6 +1,17 @@
 const Table = require("@saltcorn/data/models/table");
 const View = require("@saltcorn/data/models/view");
 const { fieldProperties } = require("../common");
+const { getState } = require("@saltcorn/data/db/state");
+const {
+  div,
+  pre,
+  code,
+  a,
+  text,
+  escape,
+  iframe,
+  text_attr,
+} = require("@saltcorn/markup/tags");
 
 class GenerateViewSkill {
   static skill_name = "Generate View";
@@ -17,14 +28,42 @@ class GenerateViewSkill {
     return `If the user asks to generate a view, use the generate_view tool to enter 
 a view generation mode. The tool call only requires high-level details to start this sequence.`;
   }
+
+  get userActions() {
+    return {
+      async build_copilot_view_gen({
+        wfctx,
+        name,
+        viewpattern,
+        table,
+        min_role,
+      }) {
+        await View.create({
+          name,
+          viewtemplate: viewpattern,
+          table: Table.findOne({ name: table }),
+          min_role: { admin: 1, public: 100, user: 80 }[min_role],
+          configuration: wfctx,
+        });
+        setTimeout(() => getState().refresh_views(), 200);
+        return {
+          notify: `Page saved: <a target="_blank" href="/view/${name}">${name}</a>`,
+        };
+      },
+    };
+  }
+
   provideTools = () => {
     const state = getState();
     const vts = state.viewtemplates;
     const tables = state.tables;
     const all_vt_names = Object.keys(vts);
     const enabled_vt_names = all_vt_names.filter(
-      (vtnm) => vts[vtnm].enable_copilot_viewgen,
+      (vtnm) =>
+        vts[vtnm].enable_copilot_viewgen ||
+        vts[vtnm].copilot_generate_view_prompt,
     );
+    //const roles = await User.get_roles();
     const parameters = {
       type: "object",
       required: ["name", "viewpattern", "table"],
@@ -45,17 +84,18 @@ a view generation mode. The tool call only requires high-level details to start 
         },
         min_role: {
           description:
-            "The minimum role needed to access the view. For vies accessible only by admin, use 'admin', pages with min_role 'public' is publicly accessible and also available to all users",
+            "The minimum role needed to access the view. For views accessible only by admin, use 'admin', pages with min_role 'public' is publicly accessible and also available to all users",
           type: "string",
-          enum: roles.map((r) => r.role),
+          enum: ["admin", "user", "public"],
         },
       },
     };
     return {
       type: "function",
       function: {
-        name: GenerateTables.function_name,
-        description: GenerateTables.description,
+        name: "generate_view",
+        description:
+          "Generate a view by supplying high-level details. This will trigger a view generation sequence",
         parameters,
       },
       process: async (input) => {
@@ -67,6 +107,13 @@ a view generation mode. The tool call only requires high-level details to start 
         const vt = state.viewtemplates[tool_call.input.viewpattern];
         const flow = vt.configuration_workflow(req);
         const wfctx = { viewname: tool_call.input.name, table_id: table?.id };
+        let vt_prompt = "";
+        if (vt.copilot_generate_view_prompt) {
+          if (typeof vt.copilot_generate_view_prompt === "string")
+            vt_prompt = vt.copilot_generate_view_prompt;
+          else if (typeof vt.copilot_generate_view_prompt === "function")
+            vt_prompt = await vt.copilot_generate_view_prompt(tool_call.input);
+        }
         for (const step of flow.steps) {
           const form = await step.form(wfctx);
           const properties = {};
@@ -78,8 +125,10 @@ a view generation mode. The tool call only requires high-level details to start 
               ...fieldProperties(field),
             };
           }
+          console.log("proerties", JSON.stringify(properties, null, 2));
+
           const answer = await generate(
-            `Now generate the ${step.name} details of the view by calling the generate_view_details tool`,
+            `${vt_prompt ? vt_prompt + "\n\n" : ""}Now generate the ${step.name} details of the view by calling the generate_view_details tool`,
             {
               tools: [
                 {
@@ -106,6 +155,16 @@ a view generation mode. The tool call only requires high-level details to start 
           console.log("step tool call result", tc.input);
           Object.assign(wfctx, tc.input);
         }
+        return {
+          stop: true,
+          add_response: pre(JSON.stringify(wfctx, null, 2)),
+          add_user_action: {
+            name: "build_copilot_view_gen",
+            type: "button",
+            label: "Save view " + tool_call.input.name,
+            input: { wfctx, ...tool_call.input },
+          },
+        };
       },
     };
   };
